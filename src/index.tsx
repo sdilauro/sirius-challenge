8dbd288dbbfe2e42a25b0e4d91031fb487c112f3
Sebastian Di Lauro <jsdilauro@gmail.com>
2022-07-25 20:09:16 -0300

    Converting to PWA

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <React.StrictMode >
+  <React.StrictMode>
     <App />
   </React.StrictMode>
 )
@@ -14,3 +15,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+serviceWorkerRegistration.register()
